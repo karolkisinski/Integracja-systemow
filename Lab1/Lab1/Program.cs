58bb6d5d78f4edebfd43ad3b1be6de1f28bbd1d4
karolkisinski <karol.kisinski@wp.pl>
2022-03-07 19:57:13 +0100

    First commit

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+
+namespace lab1_kk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string xmlpath = Path.Combine("Assets", "data.xml");
+            Console.WriteLine("XML loaded by DOM Approach");
+            XMLReadWithDOMApproach.LiczeniePostaci(xmlpath);
+            /*
+            // odczyt danych z wykorzystaniem SAX
+            Console.WriteLine("XML loaded by SAX Approach");
+            XMLReadWithSAXApproach.Read(xmlpath);
+            */
+            /*
+            // odczyt danych z wykorzystaniem XPath i DOM
+            Console.WriteLine("XML loaded with XPath");
+            XMLReadWithXLSTDOM.Read(xmlpath);
+            */
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab1/Lab1/XMLReadWithDOMApproach.cs
@@ -0,0 +1,72 @@
+﻿using System.Xml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lab1_kk
+{
+    internal class XMLReadWithDOMApproach
+    {
+        internal static void Read(string filepath)
+        {
+            // odczyt zawartości dokumentu
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filepath);
+            string postac;
+            string sc;
+            int count = 0;
+            var drugs = doc.GetElementsByTagName("produktLeczniczy");
+            foreach (XmlNode d in drugs)
+            {
+                postac = d.Attributes.GetNamedItem("postac").Value;
+                sc = d.Attributes.GetNamedItem("nazwaPowszechnieStosowana").Value;
+                if (postac == "Krem" && sc == "Mometasoni furoas")
+                    count++;
+            }
+            Console.WriteLine("Liczba produktów leczniczych w postaci kremu, których jedyną substancją czynną jest Mometasoni furoas {0}", count);
+        }
+
+        internal static void LiczeniePostaci(string filepath)
+        {
+            // odczyt zawartości dokumentu
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filepath);
+            int temp_licznik = 0;
+            string temp, sc, postac;
+            List<string> uzyte = new List<string>();
+            List<string> uzyte2 = new List<string>();
+            var drugs = doc.GetElementsByTagName("produktLeczniczy");
+            foreach (XmlNode d in drugs)
+            {
+                sc = d.Attributes.GetNamedItem("nazwaPowszechnieStosowana").Value;
+                postac = d.Attributes.GetNamedItem("postac").Value;
+                temp = sc + " %% " + postac;
+                uzyte.Add(temp);
+            }
+            uzyte.Sort();
+            uzyte = uzyte.Distinct().ToList();
+            List<string> liczenie = new List<string>();
+            foreach (string name in uzyte)
+            {
+                int index = name.IndexOf("%%");
+                liczenie.Add(name.Substring(0, index));
+            }
+            var q = liczenie.GroupBy(x => x)
+                        .Select(g => new { Value = g.Key, Count = g.Count() })
+                        .OrderByDescending(x => x.Count);
+
+            foreach (var x in q)
+            {
+                if(x.Count > 1)
+                {
+                    Console.WriteLine("Value: " + x.Value + " Count: " + x.Count);
+                    temp_licznik++;
+                }
+            }
+            Console.WriteLine("Ilosc preparatow majacych wiecej niz 1 postac o tej samej nazwie powszechnej: {0}", temp_licznik);
+        }
+
+
+
+    }
+}
